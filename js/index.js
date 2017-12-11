11afd74780ca2465b433bacde804fe9e5464101c
ALISA0710 <alisa_0710@163.com>
2017-12-11 11:33:24 +0800

    选项卡

--- js/index.js
+++ js/index.js
@@ -1,6 +1,25 @@
 //start!
-$(document).ready(function(){
-
-	
+$(document).ready(function () {
+    // 影片时间选项卡
+    $("#film-charts-time>div").click(function () {
+        $(this).css("color", "#fff").siblings().css("color", "#ba3a52");
+    });
+    // 地图选项卡
+    $("#film-charts-map>div").click(function () {
+        $(this).css("color", "#fff").siblings().css("color", "#ba3a52");
+    });
+    // top选项
+    $(".top-continer .top-p").click(function () {
+        $(this).css({
+            "background-color": "#dd2556",
+            "color":" #fff",
+            "font-size": "0.22rem"
+        }).siblings().css({
+            "background-color": "##f0f0f0",
+            "color":" #000",
+            "font-size": "0.16rem"
+        });
+    });
+    // 
 });
 //end!
