5c8da414ebdc547e95636b24524702d3fd29a5e6
ALISA0710 <alisa_0710@163.com>
2017-12-13 17:41:09 +0800

    12.13

--- js/offline-chart-circle-charts.js
+++ js/offline-chart-circle-charts.js
@@ -1,34 +1,32 @@
 //start!
 $(document).ready(function () {
-    
-        var myChart = echarts.init(document.getElementById("offline-chart-circle"));
-    
-    
-        var option3 = {
-            series : [
-                {
-                    name: '访问来源',
-                    type: 'pie',
-                    radius : '100%',
-                    center: ['50%', '50%'],
-                    data:[690,310],
-                    itemStyle: {
-                        emphasis: {
-                            shadowBlur: 10,
-                            shadowOffsetX: 0,
-                            // shadowColor: 'rgba(0, 0, 0, 0.5)'
-                        }
-                    },
-                     color:['#e02a5b','#474b57',],
-                    labelLine: {
-        normal: {
-        show: false
-        }
-        }
+
+    var myChart = echarts.init(document.getElementById("offline-chart-circle"));
+
+
+    var option3 = {
+        series: [{
+            name: '访问来源',
+            type: 'pie',
+            radius: '100%',
+            center: ['50%', '50%'],
+            data: [690, 310],
+            itemStyle: {
+                emphasis: {
+                    shadowBlur: 10,
+                    shadowOffsetX: 0,
+                    // shadowColor: 'rgba(0, 0, 0, 0.5)'
                 }
-            ]
-        };
-    
-        myChart.setOption(option3);
-    });
-    //end!
+            },
+            color: ['#e02a5b', '#474b57', ],
+            labelLine: {
+                normal: {
+                    show: false
+                }
+            }
+        }]
+    };
+
+    myChart.setOption(option3);
+});
+//end!
---
+++ js/zhexian-m-phone-charts.js
@@ -0,0 +1,133 @@
+//start!
+$(document).ready(function () {
+
+    var myChart = echarts.init(document.getElementById("m-phone-charts"));
+
+
+    var option4 = {
+        grid: {
+            top:'5%',
+            left: '0%',
+            right: '0%',
+            bottom: '1%',
+            containLabel: true
+        },
+        xAxis: {
+            type: 'category',
+            boundaryGap: false,
+            data: ['SUN', 'MON', 'TUE', 'WEN', 'THR', 'FRI', 'SAT'],
+            // x轴的字体样式
+            axisLabel: {
+                show: true,
+                textStyle: {
+                    color: '#a6acbc',
+                    fontSize: '17.6'
+                }
+            },
+            // 控制网格线是否显示
+            splitLine: {
+                show: true,
+                //  改变轴线颜色
+                lineStyle: {
+                    // 使用深浅的间隔色
+                    color: "#5c6270",
+                    width: 1, //这里是坐标轴的宽度,可以去掉
+                }
+            },
+            // x轴的颜色和宽度
+            axisLine: {
+                lineStyle: {
+                    color: '#5c6270',
+                    width: 3, //这里是坐标轴的宽度,可以去掉
+                }
+            },
+            // axisLine: {
+            //     show: true
+            //     },
+            // // 去除坐标轴上的刻度线
+            // axisTick: {
+            //      show: true
+            // }      
+        },
+        yAxis: {
+            type: 'time',
+            axisLabel: {
+                formatter: '{time} min'
+            },
+            splitLine: {　　　　
+                show: true,
+                //  改变轴线颜色
+                lineStyle: {
+                    // 使用深浅的间隔色
+                    color: "#5c6270",
+                    width: 1, //这里是坐标轴的宽度,可以去掉
+                }
+            },
+            // y轴的字体样式
+            axisLabel: {
+                show: true,
+                textStyle: {
+                    color: '#a6acbc',
+                    fontSize: '10'
+                }
+            },
+            // y轴的颜色和宽度
+            axisLine: {
+                lineStyle: {
+                    color: '#5c6270',
+                    width: 1, //这里是坐标轴的宽度,可以去掉
+                }
+            }
+        },
+        series: [{
+                type: 'line',
+                areaStyle: {
+                    // 折线区域渐变
+                    normal: {
+                        color: new echarts.graphic.LinearGradient(
+                            0, 0, 0, 1, [{
+                                    offset: 0,
+                                    color: '#d22a5d'
+                                },
+                                {
+                                    offset: 0.2,
+                                    color: '#453043'
+                                }
+                            ]
+                        )
+                    }
+                },
+                itemStyle: {
+                    normal: {
+                        color: '#fff',
+                        lineStyle: {
+                            // 折线颜色
+                            color: '#d22a5d'
+                        }
+                    }
+                },
+                data: [9268, 9632, 46547, 104332, 136993, 111545, 90897],
+                markPoint: {
+                    data: [{
+                            type: 'value',
+                            name: 'value'
+                        },
+                        {
+                            type: 'value',
+                            name: 'value'
+                        }
+                    ]
+                },
+                label: {
+                    normal: {
+                        show: true
+                    }
+                },
+            },
+
+        ]
+    };
+
+    myChart.setOption(option4);
+});
+//end!
