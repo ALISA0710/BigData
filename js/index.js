0aace3927843780627a0a20c2f0407fbaea5e6c6
ALISA0710 <alisa_0710@163.com>
2017-12-27 09:50:47 +0800

    修改tab

--- js/index.js
+++ js/index.js
@@ -31,34 +31,25 @@ $(document).ready(function () {
     $("#film-charts-map div").click(function () {
         $(this).css("color", "#fff").siblings().css("color", "#ba3a52");
         if ($(this).attr('data-id') === "country") {
+            $(".quality").css("display", "none");
             $("#country1").css("display", "block");
-            $("#city1").css("display", "none");
-            $("#company1").css("display", "none");
+            $(".number").css("display", "none");
             $("#country2").css("display", "block");
-            $("#city2").css("display", "none");
-            $("#company2").css("display", "none");
+            $(".circle-all").css("display", "none");
             $("#country3").css("display", "block");
-            $("#city3").css("display", "none");
-            $("#company3").css("display", "none");
         } else if ($(this).attr('data-id') === "city") {
-            $("#country1").css("display", "none");
+            $(".quality").css("display", "none");
             $("#city1").css("display", "block");
-            $("#company1").css("display", "none");
-            $("#country2").css("display", "none");
+            $(".number").css("display", "none");
             $("#city2").css("display", "block");
-            $("#company2").css("display", "none");
-            $("#country3").css("display", "none");
+            $(".circle-all").css("display", "none");
             $("#city3").css("display", "block");
-            $("#company3").css("display", "none");
         } else if ($(this).attr('data-id') === "company") {
-            $("#country1").css("display", "none");
-            $("#city1").css("display", "none");
+            $(".quality").css("display", "none");
             $("#company1").css("display", "block");
-            $("#country2").css("display", "none");
-            $("#city2").css("display", "none");
+            $(".number").css("display", "none");
             $("#company2").css("display", "block");
-            $("#country3").css("display", "none");
-            $("#city3").css("display", "none");
+            $(".circle-all").css("display", "none");
             $("#company3").css("display", "block");
         }
     });
@@ -67,50 +58,24 @@ $(document).ready(function () {
         $(this).addClass('choose').siblings().removeClass('choose');
         // 8.3people.html----------------------地图切换
         if ($(this).attr('data-id') === "asia") {
-            $("#asia").css("display", "block");
-            $("#n-america").css("display", "none");
-            $("#s-america").css("display", "none");
-            $("#europe").css("display", "none");
-            $("#australia").css("display", "none");
-            $("#afirca").css("display", "none");
+            $(".circle-data").css("display", "none");
+            $("#asia").css("display", "block");           
         } else if ($(this).attr('data-id') === "n-america") {
-            $("#asia").css("display", "none");
-            $("#n-america").css("display", "block");
-            $("#s-america").csis("display", "none");
-            $("#europe").css("display", "none");
-            $("#australia").css("display", "none");
-            $("#afirca").css("display", "none");
-            
+            $(".circle-data").css("display", "none");
+            $("#n-america").css("display", "block");   
         } else if ($(this).attr('data-id') === "s-america") {
-            $("#asia").css("display", "none");
-            $("#n-america").css("display", "none");
+            $(".circle-data").css("display", "none");
             $("#s-america").css("display", "block");
-            $("#europe").css("display", "none");
-            $("#australia").css("display", "none");
-            $("#afirca").css("display", "none");
         } else if ($(this).attr('data-id') === "europe") {
-            $("#asia").css("display", "none");
-            $("#n-america").css("display", "none");
-            $("#s-america").css("display", "none");
-            $("#europe").css("display", "block");
-            $("#australia").css("display", "none");
-            $("#afirca").css("display", "none");
+            $(".circle-data").css("display", "none");
+            $("#europe").css("display", "block");;
         } else if ($(this).attr('data-id') === "australia") {
-            $("#asia").css("display", "none");
-            $("#n-america").css("display", "none");
-            $("#s-america").css("display", "none");
-            $("#europe").css("display", "none");
+            $(".circle-data").css("display", "none");
             $("#australia").css("display", "block");
-            $("#afirca").css("display", "none");
         } else if ($(this).attr('data-id') === "afirca") {
-            $("#asia").css("display", "none");
-            $("#n-america").css("display", "none");
-            $("#s-america").css("display", "none");
-            $("#europe").css("display", "none");
-            $("#australia").css("display", "none");
+            $(".circle-data").css("display", "none");
             $("#afirca").css("display", "block");
         }
-
     });
 });
 //end!
