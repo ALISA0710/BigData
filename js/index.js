4bf5751344e38955aa8546c3ea01048dcb77efe4
ALISA0710 <alisa_0710@163.com>
2017-12-27 10:28:24 +0800

    第三页地图定时切换

--- js/index.js
+++ js/index.js
@@ -27,55 +27,64 @@ $(document).ready(function () {
     $(".year").click(function () {
         $(this).css("color", "#fff").siblings().css("color", "#ba3a52");
     });
-    // -----5.country---city---company  
+    // -----5.country---city---company 
+    // $(function () {
+    //     var index=0;
+    //     var top=$('.circle-data');
+
+    //     function run(){
+    //         top.hide();
+    //         top.eq(index).show();
+    //         index=(index+1)%top.length;
+    //     }
+
+    //     run();
+    //     setInterval(run,2500);
+    // });
+    // 第三页------------地图定时切换
+    $(function () {
+        var index1=0;
+        var index2=0;
+        var continent=$('.top-a');
+        var b=$('.cir-b');
+
+        function con(){
+            continent.removeClass('choose');
+            continent.eq(index1).addClass('choose');
+            index1=(index1+1)%continent.length;
+            b.hide();
+            b.eq(index2).show();
+            index2=(index2+1)%b.length;
+        }
+        con();
+        setInterval(con,2500);
+    });
+
     $("#film-charts-map div").click(function () {
         $(this).css("color", "#fff").siblings().css("color", "#ba3a52");
         if ($(this).attr('data-id') === "country") {
             $(".quality").css("display", "none");
             $("#country1").css("display", "block");
-            $(".number").css("display", "none");
+            $(".num").css("display", "none");
             $("#country2").css("display", "block");
             $(".circle-all").css("display", "none");
             $("#country3").css("display", "block");
         } else if ($(this).attr('data-id') === "city") {
             $(".quality").css("display", "none");
             $("#city1").css("display", "block");
-            $(".number").css("display", "none");
+            $(".num").css("display", "none");
             $("#city2").css("display", "block");
             $(".circle-all").css("display", "none");
             $("#city3").css("display", "block");
         } else if ($(this).attr('data-id') === "company") {
             $(".quality").css("display", "none");
             $("#company1").css("display", "block");
-            $(".number").css("display", "none");
+            $(".num").css("display", "none");
             $("#company2").css("display", "block");
             $(".circle-all").css("display", "none");
             $("#company3").css("display", "block");
         }
     });
-    // -----8.top-p    
-    $(".top-p").click(function () {
-        $(this).addClass('choose').siblings().removeClass('choose');
-        // 8.3people.html----------------------地图切换
-        if ($(this).attr('data-id') === "asia") {
-            $(".circle-data").css("display", "none");
-            $("#asia").css("display", "block");           
-        } else if ($(this).attr('data-id') === "n-america") {
-            $(".circle-data").css("display", "none");
-            $("#n-america").css("display", "block");   
-        } else if ($(this).attr('data-id') === "s-america") {
-            $(".circle-data").css("display", "none");
-            $("#s-america").css("display", "block");
-        } else if ($(this).attr('data-id') === "europe") {
-            $(".circle-data").css("display", "none");
-            $("#europe").css("display", "block");;
-        } else if ($(this).attr('data-id') === "australia") {
-            $(".circle-data").css("display", "none");
-            $("#australia").css("display", "block");
-        } else if ($(this).attr('data-id') === "afirca") {
-            $(".circle-data").css("display", "none");
-            $("#afirca").css("display", "block");
-        }
-    });
+
 });
 //end!
