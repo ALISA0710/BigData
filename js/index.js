28cb425ddbebbdef6443a9c96868b9183e75d057
ALISA0710 <alisa_0710@163.com>
2017-12-29 11:52:41 +0800

    第一页----排行定时切换--数据连接

--- js/index.js
+++ js/index.js
@@ -4,15 +4,6 @@ $(document).ready(function () {
     // -----1.day
     $(".day").click(function () {
         $(this).css("color", "#fff").siblings().css("color", "#ba3a52");
-
-        // 1.1film-day
-        // if ($(this).attr('id') === "film-day") {
-        //     $("#sw-zhexian1").css("display", "none");
-
-        // }
-        //  else if ($(this).attr('id') === "film-week") {
-        //     $("#sw-zhexian1").css("display", "none");  
-        // }
     });
     // -----2.week
     $(".week").click(function () {
@@ -28,41 +19,6 @@ $(document).ready(function () {
         $(this).css("color", "#fff").siblings().css("color", "#ba3a52");
     });
     // -----5.country---city---company 
-
-    // 第三页------------地图定时切换
-
-    var index1 = 0;
-    var index2 = 0;
-
-    var tab = $('.top>div'); // 导航栏   
-    var b = $('.cir-b'); // 标数据
-
-    function RunTab() {
-        tab.removeClass('choose').eq(index1).addClass('choose');
-        index1 = (index1 + 1) % tab.length;
-
-        b.hide().eq(index2).show();
-        index2 = (index2 + 1) % b.length;
-
-        var _valueA = ['5569', '333428', '595756', '33234', '333428', '595756'];// 传参
-        var _nameB = ['.top-AS', '.top-NA', '.top-SA', '.top-EU', '.top-AU', '.top-AF'];
-        for (var i = 0; i < _nameB.length; i++) {
-            $(_nameB[i]).html(_valueA[i]);
-        };
-    }
-
-    RunTab();
-    setInterval(RunTab, 2500);
-
-    // 第四页------------宽度占比
-    function userWidth() {
-        var _valueA = ['27.3', '26.8', '23.4', '22.5'];// 传参
-        var _nameA = ['.vive-focus-value', '.gear-vr-value', '.htc-vive-value', '.oculus-value'];
-        for (var i = 0; i < _nameA.length; i++) {
-            $(_nameA[i]).width(_valueA[i] + '%').html(_valueA[i] + '%');
-        };
-    }
-    userWidth();
 
     // 第一页------------城市选项卡
     function mapTab() {
@@ -98,28 +54,74 @@ $(document).ready(function () {
         }
     }
     mapTab();
-    // 排行定时切换
+    //----------排行定时切换--数据连接
+
     var index = 0;
     var top = $(".topa");
-    var index3 = 0;
-    var cirD = $(".circle-data");
-
+    var cirD = $(".circle-name");
+    // 坐标名字
+    var cirE = $(".circle-data");
+    // 坐标块
+    var cirF = $(".top-c");
+    // 坐标数值
+    var cirValueNum = ['5569', '333428', '595756', '33234', '333428', '595756',
+        '5569', '333428', '595756', '33234', '333428', '595756',
+        '5569', '333428', '595756', '33234', '333428', '595756',
+        '5569', '333428', '595756', '33234', '333428', '595756',
+        '5569', '333428', '595756', '33234', '333428', '595756',
+        '5569', '333428', '595756', '33234', '333428', '595756'
+    ]; // 传参
     function twoValue() {
+        var current = top.eq(index);
         top.removeClass('choose').eq(index).addClass('choose');
         index = (index + 1) % top.length;
-        cirD.hide().eq(index3).show();
-        index3= (index3 + 1) % cirD.length;
-       
+        var currentCityName = current.html();
+        for (var i = 0; i < cirD.length; i++) {
+            // 判断关联
+            if (cirD.eq(i).html() == currentCityName) {
+                cirE.eq(i).show();
+                cirF.eq(i).html(cirValueNum[i]);
+            } else {
+                cirE.eq(i).hide();
+            }
+        }
     }
     twoValue();
     setInterval(twoValue, 2500);
-    $(function(){
-        $("span[class^='topa']").each(function(i) {
-            $(this).html();
-            $(this).attr('class');
-            });
-    
-    });
-    
+
+    // 第三页------------地图定时切换
+
+    var index1 = 0;
+    var index2 = 0;
+
+    var tab = $('.top-b'); // 导航栏   
+    var b = $('.cir-b'); // 标数据
+
+    function RunTab() {
+        tab.removeClass('choose').eq(index1).addClass('choose');
+        index1 = (index1 + 1) % tab.length;
+
+        b.hide().eq(index2).show();
+        index2 = (index2 + 1) % b.length;
+
+        var _valueA = ['5569', '333428', '595756', '33234', '333428', '595756']; // 传参
+        var _nameB = ['.top-AS', '.top-NA', '.top-SA', '.top-EU', '.top-AU', '.top-AF'];
+        for (var i = 0; i < _nameB.length; i++) {
+            $(_nameB[i]).html(_valueA[i]);
+        };
+    }
+
+    RunTab();
+    setInterval(RunTab, 2500);
+
+    // 第四页------------宽度占比
+    function userWidth() {
+        var _valueA = ['27.3', '26.8', '23.4', '22.5']; // 传参
+        var _nameA = ['.vive-focus-value', '.gear-vr-value', '.htc-vive-value', '.oculus-value'];
+        for (var i = 0; i < _nameA.length; i++) {
+            $(_nameA[i]).width(_valueA[i] + '%').html(_valueA[i] + '%');
+        };
+    }
+    userWidth();
 });
 //end!
